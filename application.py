6ddc3d4adf44bfb9882e34496fbde184338cc821
Hatim Belhadjhamida <hatim-1234@hotmail.com>
2020-01-11 19:38:55 +0000

    Added application.py

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+import json
+import re
+
+application = Flask(__name__)
+
+logger = logging.getLogger()
+
+@application.route('/')
+def hello():
+    return "Hello"
+
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.templates_auto_reload = True
+    application.run()
